252985342bb817cbace6c427113d02d1064da913
luft <araryap@gmail.com>
2024-11-23 21:52:34 +0700

    update

---
+++ README.md
@@ -0,0 +1,15 @@
+**Simplified Eling and HackerRank Grade Picker**
+
+---
+
+## To Do
+1. Enter student names in `data/nama.txt` in order.
+2. Enter student NIMs in `data/nim.txt` in order.
+3. Enter student usernames in `data/username.txt` in order.
+
+## How to Run
+1. Make sure that the `nama`, `nim`, and `username` files are set up properly.
+2. Open the leaderboard page or the grade page.
+3. Copy the text from the page, ensuring there are no strange strings copied along with it.
+4. Paste the text into `raw/plain.txt`.
+5. Run `python eling.py` or `python hc.py` depending on the platform.
--- eling.py
+++ eling.py
@@ -1,11 +1,11 @@
-# Read the input files
-with open('input/nama.txt', 'r') as f:
+# Read the data files
+with open('data/nama.txt', 'r') as f:
     nama_list = f.read().lower().splitlines()
 
-with open('input/nim.txt', 'r') as f:
+with open('data/nim.txt', 'r') as f:
     nim_list = f.read().splitlines()
 
-with open('input/plain.txt', 'r') as f:
+with open('raw/plain.txt', 'r') as f:
     plain_text = f.read().lower().split("\n \n")
 
 data_list = [{'nama': nama.lower(), 'nim': nim, 'nilai': '0'} for nama, nim in zip(nama_list, nim_list)]
@@ -22,10 +22,10 @@ for index, data in enumerate(plain_text):
         if entry['nim'] == nim:
             entry['nilai'] = nilai
 
-with open('output/grade.txt', 'w') as f:
+with open('result/grade.txt', 'w') as f:
     for item in data_list:
         f.write(f"{item['nilai']}\n")
 
-with open('output/desc.txt', 'w') as f:
+with open('result/desc.txt', 'w') as f:
     for item in data_list:
         f.write(f"{item['nim']}\t{item['nama']}\t{item['nilai']}\n")
